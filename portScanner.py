191a5dedebb68ed0085928a4808391afece4c4d1
unknown <bisiceteasec@gmail.com>
2021-11-11 12:54:23 +0700

    Done

--- portScanner.py
+++ portScanner.py
@@ -1,5 +1,31 @@
+#!/usr/bin/python3
 import sys
-host = sys.argv[1]
-portstrs = sys.argv[2].split('-')
-start = int(portstrs[0])
-end = int(portstrs[1])
+import _thread
+import time
+from socket import *
+
+def tcp_test(port):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.settimeout(10)
+    result = sock.connect_ex((targetIP, port))
+    if result == 0:
+        lock.acquire()
+        print(str(port) + ' ', end = '')
+        lock.release()
+        _thread.exit()
+
+if __name__=='__main__':
+    host = sys.argv[1]
+    portRange = sys.argv[2].split('-')
+
+    startPort = int(portRange[0])
+    endPort = int(portRange[1])
+
+    targetIP = gethostbyname(host)
+
+    lock = _thread.allocate_lock()
+    print('IP: %s'%(targetIP))
+    print("Opened ports: ", end = '')
+    for port in range(startPort, endPort):
+        _thread.start_new_thread(tcp_test, (port,))
+        time.sleep(1)
